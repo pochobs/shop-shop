e9f46881c6a0fd195c77d5bdde7bc4079709edaa
Pochobs <pochobs@hotmail.com>
2020-10-17 21:16:00 -0500

    Module 22.3.5

--- client/src/components/ProductList/index.js
+++ client/src/components/ProductList/index.js
@@ -7,6 +7,7 @@ import spinner from "../../assets/spinner.gif"
 
 import { useStoreContext } from '../../utils/GlobalState';
 import { UPDATE_PRODUCTS } from '../../utils/actions';
+import { idbPromise } from "../../utils/helpers";
 
 function ProductList() {
   const [state, dispatch] = useStoreContext();
@@ -21,8 +22,22 @@ function ProductList() {
         type: UPDATE_PRODUCTS,
         products: data.products
       });
-    }
-  }, [data, dispatch]);
+          // but let's also take each product and save it to IndexedDB using the helper function 
+      data.products.forEach((product) => {
+        idbPromise('products', 'put', product);
+      });
+        // add else if to check if `loading` is undefined in `useQuery()` Hook
+      } else if (!loading) {
+        // since we're offline, get all of the data from the `products` store
+        idbPromise('products', 'get').then((products) => {
+          // use retrieved data to set global state for offline browsing
+          dispatch({
+            type: UPDATE_PRODUCTS,
+            products: products
+          });
+        });
+  }
+  }, [data, loading, dispatch]);
   
   function filterProducts() {
     if (!currentCategory) {
